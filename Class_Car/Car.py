4fe1f15bd30e7ff3601de3b96ed902bc9278ec5e
alpezel <allistairevillaruel@gmail.com>
2023-07-01 19:50:37 +0800

    add methods for acceleration and brake, getter method for speed

--- Class_Car/Car.py
+++ Class_Car/Car.py
@@ -4,5 +4,18 @@ class Car:
         self.__year_model= year_model
         self.__make= make
         self.__speed= 0
+    
+    # Method for Acceleration
+    def accelerate(self):
+        self.__speed += 5
 
+    # Method for Brake
+    def brake(self):
+        self.__speed -= 5
+        if self.__speed < 0:
+            self.__speed = 0
+    # Getter Method for speed
+    def get_speed(self):
+        return self.__speed
+    
 
