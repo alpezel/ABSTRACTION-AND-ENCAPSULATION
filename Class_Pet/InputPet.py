3114c4e15d0d149c3efd5d4e2d14353b46520919
alpezel <allistairevillaruel@gmail.com>
2023-07-01 21:20:48 +0800

    change set methods to fix error when testing and add font color to the user interface

--- Class_Pet/InputPet.py
+++ Class_Pet/InputPet.py
@@ -6,9 +6,10 @@ def InputPet():
     pet = Pet("", "", 0)
 
     # Ask user for pets details with error handling
+    print(boarder)
     while True:
         try:
-            name = input("Enter the pet's name: ")
+            name = input("\033[0;47mEnter the pet's name: ")
             if not name:
                 raise ValueError("Name should not be Empty")
             break
@@ -33,4 +34,16 @@ def InputPet():
             break
         except ValueError as Error:
             print(Error)
+    
+    # Setting the Pet's details using setter method
+    pet.set_name(name)
+    pet.set_animal_type(animal_type)
+    pet.set_age(age)
+
+    # Display pet's details
+    print(boarder)
+    print("Pet's Name:", pet.get_name())
+    print("Animal Type:", pet.get_animal_type())
+    print("Pet's Age:", pet.get_age())
+    print(boarder)
 InputPet()
--- Class_Pet/Pet.py
+++ Class_Pet/Pet.py
@@ -8,20 +8,25 @@ class Pet:
     # Add Getter/Setter Methods
 
     # Name
+    def set_name(self, name):
+        self.__name = name
+    
     def get_name(self):
-        return self.__name
-    def set_name(self):
         return self.__name
     
     # Animal Type
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
     def get_animal_type(self):
         return self.__animal_type
-    def set_animal_type(self):
-        return self.__animal_type
+    
     
     # Age
+    def set_age(self,age):
+        self.__age = age
+    
     def get_age(self):
         return self.__age
-    def set_age(self):
-        return self.__age
+    
     
